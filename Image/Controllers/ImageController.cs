d76d5de0c2e1b46bd283628f7b95c3dffdf010fd
petermreid <peter_m_reid@hotmail.com>
2013-11-24 16:12:33 +1100

    missed some files

---
+++ Image/Controllers/ImageController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Text;
+using BuskerProxy.Handlers;
+using BuskerProxy.Extensions;
+
+namespace BuskerProxy.Controllers
+{
+    public class ImageController : ApiController
+    {
+        [HttpGet]
+        [Route("flip")]
+        public HttpResponseMessage Flip()
+        {
+            string content;
+            if(ImageHandler.Flip(Request.GetClientIp()))
+                content = "Flipped On";
+            else
+                content = "Flipped Off";
+            return new HttpResponseMessage(HttpStatusCode.OK) 
+                { Content = new StringContent(content, Encoding.UTF8, "text/plain") };
+        }
+
+        [HttpGet]
+        [Route("bw")]
+        public HttpResponseMessage BW()
+        {
+            string content;
+            if (ImageHandler.BW(Request.GetClientIp()))
+                content = "Black & White On";
+            else
+                content = "Black & White Off";
+            return new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(content, Encoding.UTF8, "text/plain") };
+        }
+    }
+}
